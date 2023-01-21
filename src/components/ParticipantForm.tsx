f33d73275f434dcc77cad73ddf83c6c8a21f3f7f
fhero2030 <jcarloasilo@gmail.com>
2023-01-21 22:22:22 +0800

    page for configuring user info

---
+++ src/components/ParticipantForm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ParticipantForm = () => {
+  return (
+    <form className="flex h-[70vh] w-full flex-col items-center pt-12 md:justify-center md:pt-0">
+      <div className="flex w-11/12 max-w-[24rem] flex-col border-2 border-solid border-black sm:w-96">
+        <h2 className="bg-primary p-2 text-center font-semibold text-white">
+          ALREADY REGISTERED?
+        </h2>
+      </div>
+    </form>
+  );
+};
+
+export default ParticipantForm;
---
+++ src/pages/event/[eventId]/participant.tsx
@@ -0,0 +1,9 @@
+import { type NextPage } from "next";
+import React from "react";
+import ParticipantForm from "../../../components/ParticipantForm";
+
+const Participant: NextPage = () => {
+  return <ParticipantForm />;
+};
+
+export default Participant;
