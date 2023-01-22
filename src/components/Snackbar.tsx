a86e04e3637449906723866ad26800cbb9164558
fhero2030 <jcarloasilo@gmail.com>
2023-01-22 16:36:54 +0800

    adjust alert message position

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -27,10 +27,10 @@ const Snackbar = ({ message, type, onClose }: Props) => {
   if (type === "success") {
     return (
       <div
-        className="fixed inset-0  top-auto bottom-2 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700"
+        className="fixed inset-0 top-2 bottom-auto rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700"
         role="alert"
       >
-        <p className="font-bold">Error</p>
+        <p className="font-bold">Success</p>
         <span className="block sm:inline">{message}</span>
         <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
           <IoClose onClick={onClose} className="text-xl text-green-500" />
