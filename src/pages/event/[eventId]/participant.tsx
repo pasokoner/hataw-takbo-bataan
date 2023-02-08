aa4506689893168712278cb013f99aa546ca9582
fhero2030 <jcarloasilo@gmail.com>
2023-02-08 11:03:22 +0800

    fix name issue

--- src/pages/event/[eventId]/participant.tsx
+++ src/pages/event/[eventId]/participant.tsx
@@ -232,11 +232,11 @@ const Participant: NextPage = () => {
             <div className="col-span-1 flex flex-col gap-2 md:col-span-1">
               <div>
                 <p className="font-semibold">First Name</p>
-                <p>{participant.lastName}</p>
+                <p>{participant.firstName}</p>
               </div>
               <div>
                 <p className="font-semibold">Last Name</p>
-                <p>{participant.firstName}</p>
+                <p>{participant.lastName}</p>
               </div>
             </div>
             <div className="col-span-1 flex flex-col gap-2 md:col-span-1">
@@ -335,11 +335,11 @@ const Participant: NextPage = () => {
             <div className="col-span-2 flex flex-col gap-2 md:col-span-1">
               <div className="md:mb-3">
                 <p className="font-semibold">First Name</p>
-                <p>{participant.lastName}</p>
+                <p>{participant.firstName}</p>
               </div>
               <div>
                 <p className="font-semibold">Last Name</p>
-                <p>{participant.firstName}</p>
+                <p>{participant.lastName}</p>
               </div>
             </div>
             <div className="col-span-2 flex flex-col gap-2 md:col-span-1">
